96efe4c4708f69d1da11a01a2c67e52e888e9d7e
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2020-10-06 18:08:33 +0200

    fix function spacing

--- start.py
+++ start.py
@@ -17,12 +17,14 @@ from riitag import oauth2, user, watcher, presence, preferences
 
 nest_asyncio.apply()
 
+
 # Get resource when frozen with PyInstaller
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
-	
+
+
 try:
     with open(resource_path('config.json'), 'r') as file:
         CONFIG: dict = json.load(file)
