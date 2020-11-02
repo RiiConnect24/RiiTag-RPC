9216ffdaeb4d054fbaefcbeb247dbc36f7c25655
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2020-11-02 20:12:20 +0100

    add experimental buildhoosk

---
+++ buildhooks/hook-sentry_sdk.py
@@ -0,0 +1,18 @@
+# Forked from Wiimmfi-RPC: https://github.com/DismissedGuy/wiimmfi-rpc
+
+
+hiddenimports = [
+    "sentry_sdk.integrations.stdlib",
+    "sentry_sdk.integrations.excepthook",
+    "sentry_sdk.integrations.dedupe",
+    "sentry_sdk.integrations.atexit",
+    "sentry_sdk.integrations.modules",
+    "sentry_sdk.integrations.argv",
+    "sentry_sdk.integrations.logging",
+    "sentry_sdk.integrations.threading",
+    "sentry_sdk.integrations.django",
+
+    "packaging.version",
+    "packaging.specifiers",
+    "packaging.requirements"
+]
