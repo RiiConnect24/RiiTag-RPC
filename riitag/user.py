2ba6bb3e787ea719fdb1834da43daec9c204fef3
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-05-27 19:14:33 +0200

    Update riitag api endpoint

--- riitag/user.py
+++ riitag/user.py
@@ -5,7 +5,7 @@ import requests
 
 from .exceptions import RiitagNotFoundError
 
-RIITAG_ENDPOINT = 'http://tag.rc24.xyz/{}/json'
+RIITAG_ENDPOINT = 'http://tag.rc24.xyz/api/user/{}'
 TITLES_URL = 'https://www.gametdb.com/wiitdb.txt?LANG=EN'
 HEADERS = {'User-Agent': 'RiiTag-RPC WatchThread v1'}
 
